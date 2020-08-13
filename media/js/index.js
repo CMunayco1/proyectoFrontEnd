2ef2dcb8e32d4d434ba39bf930fc393227e48c75
César Munayco <cesarmunayco@gmail.com>
2020-08-13 16:58:40 -0500

    Primer commit - index

---
+++ media/js/index.js
@@ -0,0 +1,8 @@
+let elem = document.querySelector(".main-carousel");
+let flkty = new Flickity(elem, {
+//   freeScroll: true,
+  wrapAround: true,
+  autoPlay: 3000,
+  pauseAutoPlayOnHover: false
+
+});
